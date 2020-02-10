69120b8d2012c2fdfeff590499ff0663a5dccbcf
Mihai CICU <cicumihai02@gmail.com>
2020-02-10 09:09:32 +0200

    added @angular/flex-layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatChipsModule, MatSliderModule, MatSlideToggleModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatChipsModule, MatSliderModule, MatSlideToggleModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import {MatButtonModule, MatChipsModule, MatSliderModule, MatSlideToggleModule, 
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule, MatToolbarModule, MatSlideToggleModule, MatButtonModule, MatChipsModule
+    MatSliderModule, MatToolbarModule, MatSlideToggleModule, MatButtonModule, MatChipsModule, FlexLayoutModule, MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
