8eeee28efb697ddebe51db383f08440c7d4e9896
Mihai CICU <cicumihai02@gmail.com>
2020-02-11 14:58:30 +0200

    testing display:flex and fxFlex (grow shrink basis)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatChipsModule, MatSliderModule, MatSlideToggleModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule,
+  MatChipsModule,
+  MatGridListModule, MatListModule, MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
@@ -17,7 +25,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule, MatToolbarModule, MatSlideToggleModule, MatButtonModule, MatChipsModule, FlexLayoutModule, MatTabsModule
+    MatSliderModule, MatToolbarModule, MatSlideToggleModule, MatButtonModule, MatChipsModule, FlexLayoutModule, MatTabsModule, MatGridListModule, MatSidenavModule, MatListModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
