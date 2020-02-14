e16c2b9757a4151e2bc6b59bd035dbe780ee6207
Mihai CICU <cicumihai02@gmail.com>
2020-02-14 09:52:08 +0200

    design tweaks

--- frontend/src/app/services/tutor-list.service.ts
+++ frontend/src/app/services/tutor-list.service.ts
@@ -8,8 +8,8 @@ export class TutorListService {
 
   private tutors: Tutor[] = [
     new Tutor('Mihai C.', 'https://icons-for-free.com/iconfiles/png/512/github+network+share+social+square+icon-1320086086636119316.png',['Computer Science'], 100, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.'),
-    new Tutor('Mihai C.', 'https://icons-for-free.com/iconfiles/png/512/github+network+share+social+square+icon-1320086086636119316.png',['Computer Science'], 100, 'Teaching computer science'),
-    new Tutor('Mihai C.', 'https://icons-for-free.com/iconfiles/png/512/github+network+share+social+square+icon-1320086086636119316.png',['Computer Science'], 100, 'Teaching computer science'),
+    new Tutor('Ana T.', 'https://icons-for-free.com/iconfiles/png/512/github+network+share+social+square+icon-1320086086636119316.png',['Mathematics'], 100, 'Teaching Mathematics'),
+    new Tutor('Mihai C.', 'https://icons-for-free.com/iconfiles/png/512/github+network+share+social+square+icon-1320086086636119316.png',['Computer Science'], 75, 'Teaching computer science'),
   ];
 
   constructor() {
