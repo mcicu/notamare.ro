8b78e6778d97998846a7bbca5a8c3df0603443e4
Mihai CICU <cicumihai02@gmail.com>
2020-02-21 17:11:35 +0200

    added Roboto Slab font; progress on tutor-overview component

--- frontend/src/app/dto/tutor.ts
+++ frontend/src/app/dto/tutor.ts
@@ -5,16 +5,22 @@ export class Tutor {
   subjects: string[];
   session: { price: number, duration: number };
   description: string;
+  phoneNumber: string;
+  county: string;
   stars: number;
+  preferences: { studentLevel: string[], tutoringLocations: string[] };
 
 
-  constructor(id: string, name: string, image: string, subjects: string[], session: { price: number; duration: number }, description: string, stars: number) {
+  constructor(id: string, name: string, image: string, subjects: string[], session: { price: number; duration: number }, description: string, phoneNumber: string, county: string, stars: number, preferences: { studentLevel: string[]; tutoringLocations: string[] }) {
     this.id = id;
     this.name = name;
     this.image = image;
     this.subjects = subjects;
     this.session = session;
     this.description = description;
+    this.phoneNumber = phoneNumber;
+    this.county = county;
     this.stars = stars;
+    this.preferences = preferences;
   }
 }
